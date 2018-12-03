f16492b5a863beb6625a170ecede3616529b2dea
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-03 23:38:26 +0900

    等価演算子「==」（基本型と参照型）

---
+++ chapter2/script/equal_ref.js
@@ -0,0 +1,10 @@
+console.log(1 == true); // 結果：true
+
+var data1 = ['JavaScript', 'Ajax', 'ASP.NET'];
+var data2 = ['JavaScript', 'Ajax', 'ASP.NET'];
+console.log(data == data2); // 結果：false
+
+/*
+ * 参照型では、その参照値（メモリ上のアドレス）が格納されるので、
+ * 参照値（メモリ上のアドレス）が等しい場合にのみtrueが返される。
+ */
