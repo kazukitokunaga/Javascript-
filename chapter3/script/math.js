e52b5769f4f98e19fd4dda1d73bbfbfe5d49acdb
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-06 22:28:52 +0900

    mathオブジェクト

---
+++ chapter3/script/math.js
@@ -0,0 +1,49 @@
+// 絶対値
+console.log(Math.abs(-100)); // 結果：100
+// 32ビットバイナリにおいて前にある0ビットの個数
+console.log(Math.clz32(1)); // 結果：31
+// 最も小さな値
+console.log(Math.min(20,40,60)); // 結果：20
+// 最も大きな値
+console.log(Math.max(20,40,60)); // 結果：60
+// べき乗（5の3乗）
+console.log(Math.pow(5,3)); // 結果：125
+// 0〜1未満の乱数
+console.log(Math.random); // 結果：0.13934720965325398
+// 指定した値が正数の場合は1、負数の場合は-1、0の場合は0
+console.log(Math.sign(-100)); // 結果：-1
+// 小数点以下の切り上げ
+console.log(Math.ceil(1234.56)); // 結果：1235
+console.log(Math.ceil(-1234.56)); // 結果：-1234
+// 小数点以下の切り捨て
+console.log(Math.floor(1234.56)); // 結果：1234
+console.log(Math.floor(-1234.56)); // 結果：-1235
+// 四捨五入
+console.log(Math.round(1234.56)); // 結果：1235
+console.log(Math.round(-1234.56)); // 結果：-1235
+// 小数部分を単純に切り捨て
+console.log(Math.trunc(1234.56)); // 結果：1234
+console.log(Math.trunc(-1234.56)); // 結果：-1234
+// 平方根
+console.log(Math.sqrt(81)); // 結果：9
+// 立方根
+console.log(Math.cbrt(81)); // 結果：4.326748710922225
+// 引数の2乗和の平方根
+console.log(Math.hypot(3,4)); // 結果：5
+// コサイン
+console.log(Math.cos(1)); // 結果：0.5403023058681398
+// サイン
+console.log(Math.sin(1)); // 結果：0.8414709848078965
+// タンジェント
+console.log(Math.tan(1)); // 結果：1.5574077246549023
+// アークタンジェント
+console.log(Math.atan2(1,3)); // 結果：0.3217505543966422
+// 自然対数
+console.log(Math.log(10)); // 結果：2.302585092994046
+// 指数関数
+console.log(Math.exp(3)); // 結果：20.085536923187668
+// enum-1
+console.log(Math.expm1(1)); // 結果：1.718281828459045
+
+
+
